58a85f3c848dc42962e6ddecc33e7425f381c4e7
DANTE <104479517+bintangdase@users.noreply.github.com>
2024-07-17 12:14:47 +0700

    kontol

--- sample_config.py
+++ sample_config.py
@@ -15,11 +15,11 @@ DEPLOYING_ON_HEROKU = (
 
 
 if not DEPLOYING_ON_HEROKU:
-    BOT_TOKEN = "123456:qwertyuiopasdfghjklzxcvbnm"
-    SUDOERS = [1243703097]
-    NSFW_LOG_CHANNEL = -1001470187101
-    SPAM_LOG_CHANNEL = -1001554591017
-    ARQ_API_KEY = ""  # Get it from @ARQRobot
+    BOT_TOKEN = "7382535660:AAF1W3s_g1NhQlqr_d4uFrhaByj5Nq7H-Fk"
+    SUDOERS = [6996341586]
+    NSFW_LOG_CHANNEL = -1002227725928
+    SPAM_LOG_CHANNEL = -1002214254535
+    ARQ_API_KEY = "EEQDKW-JSNCHU-YOZPVF-KOBPGR-ARQ"  # Get it from @ARQRobot
 else:
     BOT_TOKEN = env.get("BOT_TOKEN")
     SUDOERS = [int(x) for x in env.get("SUDO_USERS_ID", "").split()]
